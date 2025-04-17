0bd7b2ad6d67ec39633164b109c43f491466de69
Aiden <aidenfor21@gmail.com>
2025-04-17 11:55:27 -0500

    added config

---
+++ src/config.py
@@ -0,0 +1,13 @@
+BODY_PARTS = {
+    " ": " ",
+
+}
+
+LABELS = ["idle", "walking", "raised right arm"]
+
+DATA_DIRECTORIES = {
+    "logs": "./data/logs/",
+    "models": "./data/models/",
+    "processed": "./data/processed/",
+    "raw": "./data/raw/"
+}
