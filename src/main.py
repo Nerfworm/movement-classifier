c7226422679181f77d0b201d0fd4418b15aef390
Aiden <aidenfor21@gmail.com>
2025-04-21 20:39:34 -0500

    added variables to main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from vr_data_collector import VRDataCollector
 from config import CONNECTED_DEVICES
 
-# data_collector = VRDataCollector(CONNECTED_DEVICES, .5)
+duration_in_seconds = 5
+collection_interval_seconds = .5
+
+# data_collector = VRDataCollector(CONNECTED_DEVICES, collection_interval_seconds)
 # data_collector.collect_data_once()
-# data_collector.run(5)
+# data_collector.run(duration_in_seconds)
