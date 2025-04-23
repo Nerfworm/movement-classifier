9634ca44c33f3f68bc63b4d9ca4245ff083b48df
Aiden <aidenfor21@gmail.com>
2025-04-23 13:17:16 -0500

    Testing data collection

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from vr_data_collector import VRDataCollector
+from data_saver import DataSaver
 from config import CONNECTED_DEVICES
 
-duration_in_seconds = 5
-collection_interval_seconds = .5
+data_label = "testing"
+duration_in_seconds = 10
+collection_interval_seconds = .1
 
-# data_collector = VRDataCollector(CONNECTED_DEVICES, collection_interval_seconds)
+data_saver = DataSaver(data_label)
+data_collector = VRDataCollector(CONNECTED_DEVICES, data_saver, collection_interval_seconds)
 # data_collector.collect_data_once()
-# data_collector.run(duration_in_seconds)
+data_collector.run(duration_in_seconds)
